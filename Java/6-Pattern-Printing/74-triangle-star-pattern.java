0fd623e29aaee1c1d6e147ca931ebba9b99b2042
Guru-moorthi-n <gurumoorthi64117@gmail.com>
2026-09-08 18:52:27 +0530

    Triangle Star Pattern Program

---
+++ Java/6-Pattern-Printing/74-triangle-star-pattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = getInput.nextInt();
+
+        for (int i=0; i<num; i++){
+            System.out.println("*".repeat(i+1));
+        }
+        getInput.close();
+    }
+}
